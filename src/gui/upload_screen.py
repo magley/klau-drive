e6c120b5c9af482977d8bebd585811151ce56585
magley <magazinlazar2@gmail.com>
2023-04-17 15:52:39 +0200

    feat: Add tag support for GUI

--- src/gui/upload_screen.py
+++ src/gui/upload_screen.py
@@ -1,3 +1,4 @@
+from typing import List
 from PyQt6.QtWidgets import *
 from src.lambdas.upload_file import UploadFile, Init, ListFiles
 import json
@@ -8,11 +9,16 @@ class UploadScreen(QWidget):
         QWidget.__init__(self)
         
         self.fname: str = None
+        self.tags: List[str] = []
 
         self.win = win
         self.btn_pick: QPushButton
         self.lbl_fname = QLabel
-        self.txt_desc: QLineEdit
+        self.txt_desc: QTextEdit
+        self.txt_tag: QLineEdit
+        self.btn_tag_add: QPushButton
+        self.btn_tag_rem: QPushButton
+        self.lst_tags: QListWidget
         self.btn_upload: QPushButton
 
         self.init_gui()
@@ -22,14 +28,27 @@ class UploadScreen(QWidget):
     def init_gui(self):
         self.btn_pick = QPushButton("Select File")
         self.lbl_fname = QLabel("No File Selected")
-        self.txt_desc = QLineEdit()
+        self.txt_desc = QTextEdit()
+        self.txt_tag = QLineEdit()
+        self.btn_tag_add = QPushButton('Add Tag')
+        self.btn_tag_rem = QPushButton('Remove Tag')
+        self.lst_tags = QListWidget()
         self.btn_upload = QPushButton('Upload')
 
         self.txt_desc.setPlaceholderText("Enter description")
+        self.txt_tag.setPlaceholderText("Enter tag name")
+        self.btn_tag_add.clicked.connect(self.add_tag)
+        self.btn_tag_add.clicked.connect(self.set_btn_tag_add_enabled)
+        self.btn_tag_rem.clicked.connect(self.rem_tag)
+        self.txt_tag.textEdited.connect(self.set_btn_tag_add_enabled)
+        self.lst_tags.itemSelectionChanged.connect(self.set_btn_tag_rem_enabled)
         self.btn_pick.clicked.connect(self.pick_file)
         self.btn_pick.clicked.connect(self.set_btn_upload_enabled)
         self.btn_upload.clicked.connect(self.upload_file)
+
         self.set_btn_upload_enabled()
+        self.set_btn_tag_add_enabled()
+        self.set_btn_tag_rem_enabled()
 
 
     def make_layout(self):
@@ -38,9 +57,22 @@ class UploadScreen(QWidget):
         layout_file_pick = QHBoxLayout()
         layout_file_pick.addWidget(self.btn_pick)
         layout_file_pick.addWidget(self.lbl_fname)
-
         layout_main.addLayout(layout_file_pick)
-        layout_main.addWidget(self.txt_desc)
+
+        layout_file_details = QHBoxLayout()
+        layout_file_details.addWidget(self.txt_desc)
+
+        layout_file_tags = QVBoxLayout()
+        layout_file_tags_input = QHBoxLayout()
+        layout_file_tags_input.addWidget(self.txt_tag)
+        layout_file_tags_input.addWidget(self.btn_tag_add)
+        layout_file_tags_input.addWidget(self.btn_tag_rem)
+
+        layout_file_tags.addLayout(layout_file_tags_input)
+        layout_file_tags.addWidget(self.lst_tags)
+        layout_file_details.addLayout(layout_file_tags)
+        layout_main.addLayout(layout_file_details)
+
         layout_main.addWidget(self.btn_upload)
         layout_main.addItem(QSpacerItem(1, 1, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
 
@@ -64,11 +96,39 @@ class UploadScreen(QWidget):
             self.btn_upload.setEnabled(True)
 
 
+    def set_btn_tag_add_enabled(self):
+        if self.txt_tag.text().strip() == "":
+            self.btn_tag_add.setEnabled(False)
+        else:
+            self.btn_tag_add.setEnabled(True)  
+
+    def set_btn_tag_rem_enabled(self):
+        if len(self.lst_tags.selectedIndexes()) == 0:
+            self.btn_tag_rem.setEnabled(False)
+        else:
+            self.btn_tag_rem.setEnabled(True)
+
+
+    def add_tag(self):
+        new_tag = self.txt_tag.text()
+        self.tags.append(new_tag)
+        self.lst_tags.addItem(QListWidgetItem(new_tag))
+        self.txt_tag.setText("")
+
+
+    def rem_tag(self):
+        selected_rows = [t.row() for t in self.lst_tags.selectedIndexes()]
+        for row in selected_rows:
+            self.tags.remove(self.tags[row])
+            self.lst_tags.takeItem(row)
+
+
     def upload_file(self):
         fname: str = self.fname
-        desc: str = self.txt_desc.text()
+        desc: str = self.txt_desc.toPlainText()
+        tags: List[str] = self.tags
 
         Init()
-        UploadFile(fname, desc, [])
+        UploadFile(fname, desc, tags)
         for file in ListFiles():
             print(json.dumps(file, indent=2, default=str))
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ main_window = QStackedWidget()
 main_window.resize(640, 480)
 main_window.setWindowTitle('klau-drive')
 main_window.addWidget(UploadScreen(main_window))
-main_window.setWindowIcon(QtGui.QIcon('ico.png'))
+main_window.setWindowIcon(QtGui.QIcon('res/ico.png'))
 
 main_window.show()
 
