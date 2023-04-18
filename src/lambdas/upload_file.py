6412c4370c378f413e1260524d15c4e3b5277958
magley <magazinlazar2@gmail.com>
2023-04-18 20:49:20 +0200

    feat: Add region name when initializing boto3 session

--- src/lambdas/upload_file.py
+++ src/lambdas/upload_file.py
@@ -9,6 +9,7 @@ from dynamodb_json import json_util as d_json
 
 ACCESS_KEY = 'test'
 SECRET_KEY = 'test'
+REGION = 'us-east-1'
 ENDPOINT = 'http://localhost.localstack.cloud:4566'
 BUCKET_NAME = "content"
 TB_META_NAME = 'file_meta'
@@ -16,7 +17,8 @@ TB_META_PK = 'name'
 TB_META_SK = None
 
 session = boto3.Session(aws_access_key_id=ACCESS_KEY,
-                        aws_secret_access_key=SECRET_KEY)
+                        aws_secret_access_key=SECRET_KEY,
+                        region_name=REGION)
 s3_cli = session.client('s3', endpoint_url=ENDPOINT)
 s3_res = session.resource("s3", endpoint_url=ENDPOINT)
 dynamo_cli = session.client('dynamodb', endpoint_url=ENDPOINT)
