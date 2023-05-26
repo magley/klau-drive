e156327b2f66c1bbf8277a97a7b982cf130af38f
swopl <modaik5@gmail.com>
2023-05-26 18:36:13 +0200

    fix: Forgot broken import in register GUI

--- src/gui/register_screen.py
+++ src/gui/register_screen.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import *
-from src.service.register_user import User, register_user, list_users
+from src.service.register_user import User, register_user
 from datetime import datetime
 from src.gui.common import show_success, show_error
 import src.gui.gui_window as mainWindow
